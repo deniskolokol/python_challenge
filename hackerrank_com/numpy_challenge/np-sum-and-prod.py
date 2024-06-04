e5bab777bcc02a260b6151cbeab1789f7adf73bc
deko <dkolokol@gmail.com>
2024-06-04 13:58:12 +0200

    solved

--- hackerrank_com/numpy_challenge/np-sum-and-prod.py
+++ hackerrank_com/numpy_challenge/np-sum-and-prod.py
@@ -1,7 +1,7 @@
-# https://www.hackerrank.com/challenges/np-min-and-max/
+# https://www.hackerrank.com/challenges/np-sum-and-prod/
 
 import numpy as np
 
 N, M = map(int, input().split())
 arr = np.array([[int(x) for x in input().split(' ')] for _ in range(N)])
-print(np.min(arr, axis=1).max())
+print(np.prod(arr.sum(axis=0)))
